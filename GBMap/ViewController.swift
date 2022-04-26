78a30484a6fe3808215991002b9a2a46b9ef0eb3
RomanShichkin <oman01_93@mail.ruoman01_93@mail.ruroman01_93@mail.ru>
2022-04-26 15:57:03 +0300

    Initial commit

---
+++ GBMap/ViewController.swift
@@ -0,0 +1,107 @@
+//
+//  ViewController.swift
+//  GBMap
+//
+//  Created by Роман Шичкин on 24.04.2022.
+//
+
+import UIKit
+import GoogleMaps
+
+class ViewController: UIViewController {
+    
+    @IBOutlet weak var mapView: GMSMapView!
+    
+    let coordinate = CLLocationCoordinate2D(latitude: 37.34033, longitude: -122.06892)
+    var marker: GMSMarker?
+    var geoCoder: CLGeocoder?
+    var route: GMSPolyline?
+    var locationManager: CLLocationManager?
+    var routePath: GMSMutablePath?
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+        configureMap()
+        configureLocationManager()
+    }
+    
+    private func configureLocationManager() {
+        locationManager = CLLocationManager()
+        locationManager?.delegate = self
+        locationManager?.requestAlwaysAuthorization()
+        locationManager?.allowsBackgroundLocationUpdates = true
+    }
+    
+    private func configureMap() {
+        let camera = GMSCameraPosition.camera(withTarget: coordinate, zoom: 15)
+        mapView.camera = camera
+        mapView.isMyLocationEnabled = true
+        
+        mapView.delegate = self
+    }
+    
+    private func addMarker() {
+        marker = GMSMarker(position: coordinate)
+        marker?.icon = GMSMarker.markerImage(with: .magenta)
+        marker?.map = mapView
+    }
+    
+    @IBAction func addMarkerDidTap(_ sender: UIButton) {
+        if marker == nil {
+            mapView.animate(toLocation: coordinate)
+            addMarker()
+        } else {
+            removeMarker()
+        }
+    }
+    
+    @IBAction func didTapUpdateLocation(_ sender: UIButton) {
+        locationManager?.requestLocation()
+        locationManager?.startUpdatingLocation()
+    }
+    
+    private func removeMarker() {
+        marker?.map = nil
+        marker = nil
+    }
+    
+}
+
+extension ViewController: GMSMapViewDelegate {
+    func mapView(_ mapView: GMSMapView, didTapAt coordinate: CLLocationCoordinate2D) {
+        print(coordinate)
+        let manualMarker = GMSMarker(position: coordinate)
+        manualMarker.map = mapView
+        
+        if geoCoder == nil {
+            geoCoder = CLGeocoder()
+        }
+        geoCoder?.reverseGeocodeLocation(CLLocation(latitude: coordinate.latitude, longitude: coordinate.longitude), completionHandler: { places, error in
+            print(places?.last)
+        })
+    }
+}
+
+extension ViewController: CLLocationManagerDelegate {
+    func locationManager(_ manager: CLLocationManager, didUpdateLocations locations: [CLLocation]) {
+        guard let location = locations.last else { return }
+        
+        routePath?.add(location.coordinate)
+        route?.path = routePath
+        
+        let postition = GMSCameraPosition.camera(withTarget: location.coordinate, zoom: 15)
+        mapView.animate(to: postition)
+        
+        let manualMarker = GMSMarker(position: location.coordinate)
+        manualMarker.map = mapView
+        
+        print(location.coordinate)
+    }
+    
+    func locationManager(_ manager: CLLocationManager, didFailWithError error: Error) {
+        print(error)
+    }
+}
+
+
---
+++ Podfile
@@ -0,0 +1,21 @@
+# Uncomment the next line to define a global platform for your project
+# platform :ios, '9.0'
+
+target 'GBMap' do
+  # Comment the next line if you don't want to use dynamic frameworks
+  use_frameworks!
+
+  # Pods for GBMap
+
+  pod 'GoogleMaps'
+
+  target 'GBMapTests' do
+    inherit! :search_paths
+    # Pods for testing
+  end
+
+  target 'GBMapUITests' do
+    # Pods for testing
+  end
+
+end
